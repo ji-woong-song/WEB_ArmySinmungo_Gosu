6bf2f3cd8f3b6440f9890257e39975a529009cff
SeongHoon Jeong <swa07016@naver.com>
2021-09-19 16:11:00 +0000

    init setting

---
+++ backend/armysinmungo/src/test/java/com/gosu/armysinmungo/armysinmungo/ArmysinmungoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosu.armysinmungo.armysinmungo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmysinmungoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
