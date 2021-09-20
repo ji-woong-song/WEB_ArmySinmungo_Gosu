bed7fb1599771f02db70d908ea55972675f200e5
Thebreeze129 <52536141+TheBreeze129@users.noreply.github.com>
2021-09-20 10:56:31 +0000

    recognize some words(given by user) in sentences.

---
+++ analysis/keyword.py
@@ -0,0 +1,28 @@
+"""
+Made by TheBreeze129 (H.W. Lee)
+
+For recognize given words.
+
+"""
+
+"""
+
+to use this, maybe you have to do this.
+
+python -m pip install urllib3
+python -m pip install six
+python -m pip install requests
+
+"""
+
+#import and alias
+from konlpy.tag import Kkma
+kkma = Kkma()
+
+#pre-treat keywords
+def pre_treat_keyword(keywords):
+    return True
+
+#recognition
+def recognition(sentences, keywords):
+    return True
