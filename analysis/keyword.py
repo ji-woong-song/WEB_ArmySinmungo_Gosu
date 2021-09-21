81507fc1591eac2a33336dcc79823ce10de68277
Thebreeze129 <52536141+TheBreeze129@users.noreply.github.com>
2021-09-21 09:44:47 +0000

    recognize some words(given by usr) (algorythm)

--- analysis/keyword.py
+++ analysis/keyword.py
@@ -21,8 +21,32 @@ kkma = Kkma()
 
 #pre-treat keywords
 def pre_treat_keyword(keywords):
-    return True
+    treated_keyword = list()
+    for word in keywords:
+        treated_keyword.append(str(kkma.pos(word)[0][0]))
+    return treated_keyword
 
 #recognition
 def recognition(sentences, keywords):
-    return True
+    morphemes = kkma.pos(sentences)
+    print(morphemes)
+    morphemeslist = list()
+    recognizelist = list()
+    recognize = False
+    for set in morphemes:
+        morpheme = set[0]
+        if morpheme in keywords:
+            recognizelist.append(str(set[0]))
+            recognize = True
+        morphemeslist.append(set[0])
+
+    return recognizelist, recognize
+
+#example
+example = "너무 자살하고 싶다, 죽고싶어 진짜. 너무 힘들어"
+keywordset = ['자살', '힘들다', '죽다']
+treated_keywordset = pre_treat_keyword(keywordset)
+recongizelist, recognize= recognition(example, treated_keywordset)
+print(treated_keywordset)
+print(recongizelist)
+print(recognize)
