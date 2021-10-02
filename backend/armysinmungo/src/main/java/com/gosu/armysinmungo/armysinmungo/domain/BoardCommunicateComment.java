2e948906432f30a1d8e13b2d9c4577ba776f65b5
SeongHoon Jeong <swa07016@naver.com>
2021-10-02 14:23:36 +0000

    Create BoardCommunicateComment entity

---
+++ backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/domain/BoardCommunicateComment.java
@@ -0,0 +1,59 @@
+package com.gosu.armysinmungo.armysinmungo.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "BOARD_COMMUNICATE_COMMENT")
+@Builder @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardCommunicateComment {
+    
+    @Id @GeneratedValue
+    private Long id;
+
+    private Long mension;
+
+    @NotNull
+    @CreatedDate
+    private LocalDateTime uploadTime;
+
+    @LastModifiedDate
+    private LocalDateTime changedTime;
+
+    @NotNull
+    private String content;
+
+    @ColumnDefault("0")
+    private int agreeNum;
+
+    @ColumnDefault("0")
+    private int disagreeNum = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_INFO_ID")
+    private UserInfo userInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "BOARD_FREE_POST_ID")
+    private BoardFreePost boardFreePost;
+}
+
