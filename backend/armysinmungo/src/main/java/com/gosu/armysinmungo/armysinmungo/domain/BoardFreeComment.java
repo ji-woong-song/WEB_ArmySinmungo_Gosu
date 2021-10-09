e819fee70d2c978c3128e72e40e44a58efaa10c1
SeongHoon Jeong <swa07016@naver.com>
2021-10-09 16:03:15 +0000

    Implement BoardFreeComment api

--- backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/domain/BoardFreeComment.java
+++ backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/domain/BoardFreeComment.java
@@ -10,9 +10,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.gosu.armysinmungo.armysinmungo.web.dto.response.BoardCommentResponse;
+
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.ArrayList;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,11 +24,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.EntityListeners;
+
 @Entity
 @Table(name = "BOARD_FREE_COMMENT")
 @Builder @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class BoardFreeComment {
     
     @Id @GeneratedValue
@@ -46,7 +54,7 @@ public class BoardFreeComment {
     private int agreeNum;
 
     @ColumnDefault("0")
-    private int disagreeNum = 0;
+    private int disagreeNum;
 
     @ManyToOne
     @JoinColumn(name = "USER_INFO_ID")
@@ -55,5 +63,20 @@ public class BoardFreeComment {
     @ManyToOne
     @JoinColumn(name = "BOARD_FREE_POST_ID")
     private BoardFreePost boardFreePost;
+
+    public BoardCommentResponse toBoardCommentResponse() {
+        return BoardCommentResponse.builder()
+            .postNum(boardFreePost.getPostNum())
+            .uploadTime(uploadTime)
+            .changedTime(changedTime)
+            .userName(userInfo.getUserName())
+            .content(content)
+            .agreeNum(agreeNum)
+            .mension(mension)
+            .mensionList(new ArrayList<>())
+            .id(id)
+            .disagreeNum(disagreeNum)
+            .build();
+    }
 }
 
---
+++ backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/web/dto/response/BoardCommentResponse.java
@@ -0,0 +1,27 @@
+package com.gosu.armysinmungo.armysinmungo.web.dto.response;
+
+import java.time.LocalDateTime;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Builder
+@Getter @Setter
+public class BoardCommentResponse {
+    
+    private Long id;
+    private String content;
+    private int agreeNum;
+    private int disagreeNum;
+    private String userName;
+    private LocalDateTime uploadTime;
+    private LocalDateTime changedTime;
+    private List<BoardCommentResponse> mensionList;
+    private Long mension;
+    private int postNum;
+}
