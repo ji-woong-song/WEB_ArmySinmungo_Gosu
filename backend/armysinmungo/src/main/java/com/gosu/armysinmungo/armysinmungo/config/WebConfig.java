deb50ae8b46b23b985e5bb43e6456a99db6a5eba
SeongHoon Jeong <swa07016@naver.com>
2021-10-12 08:05:16 +0000

    Fix error

--- backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/config/WebConfig.java
+++ backend/armysinmungo/src/main/java/com/gosu/armysinmungo/armysinmungo/config/WebConfig.java
@@ -1,13 +1,11 @@
 package com.gosu.armysinmungo.armysinmungo.config;
 
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
